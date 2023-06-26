d6cf7cfa45a78e784dabdcc48a63d0c9c57ea617
leeeunmi25 <akdongsam25@naver.com>
2023-06-26 09:29:25 +0900

    0619

--- 70_project/js/script.js
+++ 70_project/js/script.js
@@ -1,77 +1,73 @@
-$(function(){
-    //프로그래스바
-    let progressState = 0;
-    
-            $(window).scroll(function () {
-                if ($(this).scrollTop() >= 150) {
-                    if (progressState === 0) {
-                        move(0, 90);
-                        move(1, 85);
-                        move(2, 75);
-                        progressState = 1;
-                    } else {
-                        return;
-                    }
-                } else {
-                    progressState = 0;
-                }
-            });
-    
-    
-            function move(idx, maxPro) {
-                const elem = $(".progress div");
-                let w = 0;
-                const timer = setInterval(bar, 30);
-    
-                function bar() {
-                    if (w >= maxPro) {
-                        clearInterval(timer);
-                    } else {
-                        w++;
-                        elem.eq(idx).css({ width: w + "%" });
-                        elem.eq(idx).text(w + "%");
-                    }
-                }
+$(function () {
+    // 프로그래스바
+    let progresssState = 0;
+
+    $(window).scroll(function () {
+        let st = $(this).scrollTop();
+        if (st >= 1500) {
+            if (progresssState === 0) {
+                pgbar(0, 90);
+                pgbar(1, 80);
+                pgbar(2, 75);
+                progresssState = 1;
+            } else {
+                return
             }
-    
-    
-    
-    /* 카운터바 */
-            const elem = $('#counter span');
-            let stateCnt = 0;
-            $(window).scroll(function () {
-                if ($(this).scrollTop() >= 2150) {
-                    if (stateCnt === 0) {
-                        move(0, 20, 100);
-                        move(1, 4, 500);
-                        move(2, 5, 400);
-                        move(3, 2, 1000);
-                        stateCnt = 1;
-                    } else {
-                        return;
-                    }
-                } else {
-                    stateCnt = 0;
-                    //  올렸다 내릴때 마다 동작
-                }
-            });
-    
-    
-            // let st = $('this').scroll;
-            function move(idx, maxCnt, mSec) {
-                let num = 0;
-                //count 함수를 0.1초마다 실행
-                const timer = setInterval(counter, mSec);
-                function counter() {
-                    if (num >= maxCnt) {
-                        clearInterval(timer);
-                    } else {
-                        num++;
-                        elem.eq(idx).text(`${num}+`);
-                    }
-                }
-            }
-    
-    
+            console.log(st)
+        } else {
+            progresssState = 0;
+        }
     });
-    
+
+    function pgbar(idx, maxCnt) {
+        var elem = $(".progress div");
+        var w = 0;
+        var timer = setInterval(bar, 15);
+        console.log(elem)
+        function bar() {
+            if (w >= maxCnt) {
+                clearInterval(timer);
+            } else {
+                w++;
+                elem.eq(idx).css({ width: w + "%" });
+                elem.eq(idx).text(w + "%");
+            }
+        }
+    }
+
+    // 카운터 바
+    const elem = $('#counter span');
+    let counterState = 0;
+    $(window).scroll(function () {
+        let st = $(this).scrollTop();
+        if (st >= 1500) {
+            if(counterState===0){
+                move(0,16,50);
+                move(1,8,100);
+                move(2,4,200);
+                move(3,2,500);
+                counterState = 1;
+            } else{
+                return;
+            }
+        } else{
+            counterState = 0;
+        }
+    });
+
+    function move(idx,maxCnt,settime) {
+        let num = 0;
+        // counter 함수를 0.1초마다 실행
+        const timer = setInterval(counter, settime);
+        
+        function counter() {
+            if (num >= maxCnt) {
+                clearInterval(timer);
+                // return;
+            } else {
+                num++;
+                elem.eq(idx).text(`${num}+`);
+            }
+        }
+    }
+});
